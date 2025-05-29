6009d59b7e143c48eb4c75d552a07c8e68bfadf1
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-05-28 19:58:17 -0600

    logout button

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: false,
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
   email: string = '';
   password: string = '';
   isLoggedIn: boolean = false;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    // Check if user is logged in when the component initializes
+    this.isLoggedIn = this.authService.isLoggedIn();
+  }
 
   login() {
     this.authService.login(this.email, this.password).subscribe({
@@ -25,8 +30,14 @@ export class NavbarComponent {
       },
       error: (error) => {
         console.error('Login failed', error);
-      }
+      },
     });
   }
 
+  logout() {
+    this.authService.logout();
+    this.isLoggedIn = false;
+    // Redirect to the login page or home page after logout
+    this.router.navigate(['/home']);
+  }
 }
